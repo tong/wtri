5485c904704c825c49caa6bd95d261ac8001123c
tong <tong@disktree.net>
2025-08-29 18:51:57 +0200

    improve filesystem handler

--- src/wtri/handler/FileSystemHandler.hx
+++ src/wtri/handler/FileSystemHandler.hx
@@ -1,14 +1,14 @@
 package wtri.handler;
 
 class FileSystemHandler implements wtri.Handler {
-	public var path:String;
+	public var root:String;
 	public var mime:Map<String, String>;
 	public var indexFileNames:Array<String>;
 	public var indexFileTypes:Array<String>;
 	public var autoIndex:Bool;
 
-	public function new(path:String, ?mime:Map<String, String>, ?indexFileNames:Array<String>, ?indexFileTypes:Array<String>, ?autoIndex = false) {
-		this.path = FileSystem.fullPath(path.trim()).removeTrailingSlashes();
+	public function new(root:String, ?mime:Map<String, String>, ?indexFileNames:Array<String>, ?indexFileTypes:Array<String>, ?autoIndex = false) {
+		this.root = FileSystem.fullPath(root.trim()).removeTrailingSlashes();
 		this.mime = mime ?? [
 			"css" => TextCss,
 			"gif" => ImageGif,
@@ -18,6 +18,7 @@ class FileSystemHandler implements wtri.Handler {
 			"jpeg" => ImageJpeg,
 			"js" => TextJavascript,
 			"json" => ApplicationJson,
+			"mp4" => "video/mp4",
 			"png" => ImagePng,
 			"svg" => "image/svg+xml",
 			"txt" => TextPlain,
@@ -33,48 +34,36 @@ class FileSystemHandler implements wtri.Handler {
 	}
 
 	public function handle(req:Request, res:Response):Bool {
-		final _path = resolvePath(req.path);
-		// TODO: check path security
-		final filePath = findFile(_path);
+		final path = resolvePath(req.path);
+		if (!path.startsWith(root)) {
+			// res.writeHead(FORBIDDEN);
+			res.code = FORBIDDEN;
+			res.data = Bytes.ofString(FORBIDDEN);
+			// res.end(Bytes.ofString("403 Forbidden"));
+			return true;
+		}
+		final filePath = findFile(path);
 		if (filePath == null) {
-			if (autoIndex) {
-				if (FileSystem.isDirectory(_path)) {
-					var html = createAutoIndex(_path, req.path);
-					res.headers.set(Content_Type, "text/html");
-					res.headers.set(Content_Length, Std.string(html.length));
-					res.data = html;
-				} else {
-					res.code = NOT_FOUND;
-				}
+			if (autoIndex && FileSystem.isDirectory(path)) {
+				final html = createAutoIndex(path, req.path);
+				res.headers.set(Content_Type, "text/html");
+				res.headers.set(Content_Length, Std.string(html.length));
+				// res.end(OK, html);
+				res.data = html;
 			} else {
+				// res.end(NOT_FOUND, Bytes.ofString(NOT_FOUND));
 				res.code = NOT_FOUND;
+				res.data = Bytes.ofString(NOT_FOUND);
 			}
 			return true;
 		}
 		res.headers.set(Content_Type, getFileContentType(filePath));
 		res.data = File.getBytes(filePath);
-		/*
-			var enc = req.getEncoding();
-			if( enc.indexOf( 'deflate' ) != -1 ) {
-				data = Deflate.run( data );
-				res.headers.set( Content_Encoding, 'deflate' );
-		}*/
-		// res.end( data );
-		/*
-			final stat = FileSystem.stat( filePath );
-			res.writeHead( [
-				'Content-type' => contentType,
-				'Content-length' => Std.string( data.length )
-				//'Content-length' => Std.string( stat.size )
-			] ); */
-		// res.writeInput( File.read( filePath ), stat.size );
-		// res.write(data);
 		return true;
 	}
 
-	function resolvePath(path:String):String {
-		return '${this.path}${path}'.normalize();
-	}
+	function resolvePath(path:String):String
+		return Path.join([root, path]).normalize();
 
 	function findFile(path:String):String {
 		if (!FileSystem.exists(path))
@@ -87,10 +76,9 @@ class FileSystemHandler implements wtri.Handler {
 	function findIndexFile(path:String):String {
 		final r = new EReg('^(${indexFileNames.join("|")})\\.(${indexFileTypes.join("|")})$', '');
 		for (f in FileSystem.readDirectory(path)) {
-			final p = '$path/$f';
-			if (!FileSystem.isDirectory(p) && r.match(f)) {
+			final p = Path.join([path, f]);
+			if (!FileSystem.isDirectory(p) && r.match(f))
 				return p;
-			}
 		}
 		return null;
 	}
@@ -104,23 +92,41 @@ class FileSystemHandler implements wtri.Handler {
 		final entries = FileSystem.readDirectory(path);
 		final directories = new Array<String>();
 		final files = new Array<String>();
-		var html = '<html><title>Index of $reqPath</title><body><pre><table>';
 		for (e in entries)
 			FileSystem.isDirectory('$path/$e') ? directories.push(e) : files.push(e);
 		function sort(a:String, b:String)
-			return (a > b) ? 1 : (a < b) ? -1 : 0;
+			return a > b ? 1 : a < b ? -1 : 0;
 		directories.sort(sort);
 		files.sort(sort);
+		final html = new StringBuf();
+		html.add("<html><title>Index of ");
+		html.add(reqPath);
+		html.add("</title><body><pre><table>");
 		for (e in directories) {
-			final lp = reqPath.removeTrailingSlashes() + '/' + e;
+			final lp = Path.join([reqPath, e]);
 			final stat = FileSystem.stat('$path/$e');
-			html += '<tr><td><a href="$lp/">$e</a></td><td>${stat.mtime}</td><td>-</td></tr>';
+			html.add('<tr><td><a href="');
+			html.add(lp);
+			html.add('/">');
+			html.add(e);
+			html.add("</a></td><td>");
+			html.add(stat.mtime);
+			html.add("</td><td>-</td></tr>");
 		}
 		for (e in files) {
-			final lp = reqPath.removeTrailingSlashes() + '/' + e;
+			final lp = Path.join([reqPath, e]);
 			final stat = FileSystem.stat('$path/$e');
-			html += '<tr><td><a href="$lp">$e</a></td><td>${stat.mtime}</td><td>-</td><td>${stat.size}</td></tr>';
+			html.add('<tr><td><a href="');
+			html.add(lp);
+			html.add('/">');
+			html.add(e);
+			html.add("</a></td><td>");
+			html.add(stat.mtime);
+			html.add("</td><td>-</td><td>");
+			html.add(stat.size);
+			html.add("</td></tr>");
 		}
-		return html + '</pre></table></body></html>';
+		html.add("</pre></table></body></html>");
+		return html.toString();
 	}
 }
