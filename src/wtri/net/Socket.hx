c3db53908b9a34b537edbb579219a54291e2145b
tong <tong@disktree.net>
2025-09-03 10:45:28 +0200

    disable buffered writing for now

--- src/wtri/net/Socket.hx
+++ src/wtri/net/Socket.hx
@@ -17,9 +17,9 @@ class TCPSocket implements Socket {
 
 	public inline function writeInput(input:haxe.io.Input, len:Int) {
 		socket.output.writeInput(input, len);
-		socket.output.flush();
+		// socket.output.flush();
 		/*
-			final chunkSize = 65536 * 32; // 64k buffer
+			final chunkSize = 65536; // 64k buffer
 			final buf = haxe.io.Bytes.alloc(chunkSize);
 			var remaining = len;
 			var chunkNum = 0;
@@ -30,16 +30,14 @@ class TCPSocket implements Socket {
 					if (bytesRead == 0)
 						break;
 					remaining -= bytesRead;
-
 					final startTime = haxe.Timer.stamp();
 					Sys.println('TCP Write Chunk #${chunkNum++}: ${bytesRead} bytes. Blocking...');
-
 					if (bytesRead == chunkSize) {
 						socket.output.write(buf);
 					} else {
 						socket.output.write(buf.sub(0, bytesRead));
 					}
-
+					// socket.output.flush();
 					final duration = haxe.Timer.stamp() - startTime;
 					Sys.println('...Unblocked after ${duration} seconds.');
 				} catch (e:haxe.io.Eof) {
@@ -64,26 +62,21 @@ class UVSocket implements Socket {
 		socket.write(data);
 
 	public inline function writeInput(input:haxe.io.Input, len:Int) {
-		final chunkSize = 65536; // 64k buffer
-		var buffer = haxe.io.Bytes.alloc(chunkSize);
-		var remaining = len;
-		while (remaining > 0) {
-			try {
-				final toRead = remaining > chunkSize ? chunkSize : remaining;
-				final bytesRead = input.readBytes(buffer, 0, toRead);
-				if (bytesRead == 0) {
-					break; // End of stream
-				}
-				remaining -= bytesRead;
-				if (bytesRead == chunkSize) {
-					socket.write(buffer);
-				} else {
-					socket.write(buffer.sub(0, bytesRead));
-				}
-			} catch (e:haxe.io.Eof) {
-				break;
-			}
-		}
+		socket.write(input.read(len));
+		// final bufSize = 65536; // 64k buffer
+		// var buf = Bytes.alloc(bufSize);
+		// var remaining = len;
+		// while (remaining > 0) {
+		//	try {
+		//		final n = input.readBytes(buf, 0, remaining > bufSize ? bufSize : remaining);
+		//		if (n == 0)
+		//			break;
+		//		remaining -= n;
+		//		n == bufSize ? socket.write(buf) : socket.write(buf.sub(0, n));
+		//	} catch (e:haxe.io.Eof) {
+		//		break;
+		//	}
+		// }
 	}
 
 	public inline function close()
